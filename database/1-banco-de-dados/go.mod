f282bdab6ad1e2f70d065b14c99f2747ed7aac4f
Danilo Y Kodavara <danilo.kodavara@hotmail.com>
2023-05-12 22:42:09 -0300

    feat: iniciando aula sobre conexao de banco de dados.

---
+++ database/1-banco-de-dados/go.mod
@@ -0,0 +1,8 @@
+module gi/home/fourleaf/develop/full-cycle/go-expert/databasethub.com/danyukod/go-expert/database
+
+go 1.20
+
+require (
+	github.com/go-sql-driver/mysql v1.7.1
+	github.com/google/uuid v1.3.0
+)
---
+++ database/1-banco-de-dados/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/google/uuid"
+	"time"
+)
+
+type Product struct {
+	ID    string
+	Name  string
+	Price float64
+}
+
+func NewProduct(name string, price float64) *Product {
+	return &Product{
+		ID:    uuid.New().String(),
+		Name:  name,
+		Price: price,
+	}
+}
+
+func main() {
+
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/goexpert")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	product := NewProduct("Go Expert", 49.90)
+	err = insertProduct(db, product)
+	if err != nil {
+		panic(err)
+	}
+	product.Price = 100.00
+	err = updateProduct(db, product)
+	if err != nil {
+		panic(err)
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Millisecond)
+	defer cancel()
+	product, err = selectOneProduct(ctx, db, product.ID)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Product: %v, possui o preço de: %.2f", product.Name, product.Price)
+}
+
+func insertProduct(db *sql.DB, product *Product) error {
+	stmt, err := db.Prepare("insert into products(id, name, price) values(?, ?, ?)")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(product.ID, product.Name, product.Price)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func updateProduct(db *sql.DB, product *Product) error {
+	stmt, err := db.Prepare("update products set name = ?, price = ? where id = ?")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(product.Name, product.Price, product.ID)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func selectOneProduct(ctx context.Context, db *sql.DB, id string) (*Product, error) {
+	stmt, err := db.Prepare("select id, name, price from products where id = ?")
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+	var product Product
+	err = stmt.QueryRowContext(ctx, id).Scan(&product.ID, &product.Name, &product.Price)
+	if err != nil {
+		return nil, err
+	}
+	return &product, nil
+}
